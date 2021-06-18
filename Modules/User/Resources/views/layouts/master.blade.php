6483344b406959c664ab578e364d1ac1e59340d9
MinhIT97 <minh06081997@gmail.com>
2021-06-18 20:15:11 +0700

    fix

--- Modules/User/Resources/views/layouts/master.blade.php
+++ Modules/User/Resources/views/layouts/master.blade.php
@@ -8,7 +8,32 @@
 
        {{-- Laravel Mix - CSS File --}}
        {{-- <link rel="stylesheet" href="{{ mix('css/user.css') }}"> --}}
+<!-- Messenger Plugin chat Code -->
+<div id="fb-root"></div>
 
+<!-- Your Plugin chat code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "100596878386543");
+  chatbox.setAttribute("attribution", "biz_inbox");
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v11.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
     </head>
     <body>
         @yield('content')
--- resources/views/pages/layouts/app_master_frontend.blade.php
+++ resources/views/pages/layouts/app_master_frontend.blade.php
@@ -1,28 +1,57 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
-        {!! SEO::generate() !!}
-        <link rel="icon" href="{{ asset('img/brand/favicon.png') }}" type="image/x-icon"/>
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script>
-            var URL_SOCIAL = '{{ route("post.social") }}'
-        </script>
-        @yield('style')
-    </head>
-    <body>
-        @include('pages.components.header._inc_header_dt')
-        @include('pages.components.header._inc_header_mb')
-        @yield('content')
-        @include('pages.components._inc_footer')
-        @if(!get_data_user('web'))
-            @include('pages.components.auth._inc_popup_auth')
-        @endif
-    </body>
-    @yield('script')
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+    {!! SEO::generate() !!}
+    <link rel="icon" href="{{ asset('img/brand/favicon.png') }}" type="image/x-icon" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script>
+        var URL_SOCIAL = '{{ route("post.social") }}'
+    </script>
+    <!-- Messenger Plugin chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "100596878386543");
+        chatbox.setAttribute("attribution", "biz_inbox");
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml: true,
+                version: 'v11.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+    @yield('style')
+</head>
+
+<body>
+    @include('pages.components.header._inc_header_dt')
+    @include('pages.components.header._inc_header_mb')
+    @yield('content')
+    @include('pages.components._inc_footer')
+    @if(!get_data_user('web'))
+    @include('pages.components.auth._inc_popup_auth')
+    @endif
+</body>
+@yield('script')
+
 </html>
